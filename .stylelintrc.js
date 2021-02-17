3649becbe4c941ffbc062e9e301e050c6edaae6f
Objectivezt <objectivezt@gmail.com>
2021-02-17 11:59:26 +0800

    feat: add stylelintrc

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: objectivezt
+ * @Date: 2021-02-17 10:35:39
+ * @Last Modified by: objectivezt
+ * @Last Modified time: 2021-02-17 11:54:59
+ */
+module.exports = {
+  extends: [
+    'stylelint-config-prettier',
+    'stylelint-config-standard',
+    './node_modules/prettier-stylelint/config.js'
+  ],
+  rules: {
+  }
+};
